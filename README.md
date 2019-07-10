9df17dfd2395795110120d8f7ccb1ecdf6dfb536
Robert Laws <roblaws@me.com>
2019-07-10 20:28:06 +0300

    form setup - before redux

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Learning - July 2019 - Building Applications with React and Redux
 
 Code along to Pluralsight course - Building Applications with React and Redux
+
+## Steps to Setup and Add Feature to Redux
+
+### Setup
+
+1. create action
+1. create reducer
+1. create root reducer
+1. configure store
+1. instantiate store
+1. connect component
+1. pass props via connect
+1. dispatch action
+
+### Add Feature
+
+1. create action
+1. enhance reducer
+1. connect component
+1. dispatch action
+
--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,15 +1,68 @@
 import React, { Component } from 'react';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class CoursesPage extends Component {
+  state = {
+    course: {
+      title: ''
+    }
+  }
+
+  handleChange = event => {
+    const course = { ...this.state.course, title: event.target.value }
+    this.setState({
+      course
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.course.title)
+  }
+
   render() {
     return (
-      <Box m="1rem">
-        <Typography component="h2" variant="h3" align="left" color="textPrimary" gutterBottom>
-          Courses
-        </Typography>
-      </Box>
+      <>
+        <Box m="1rem">
+          <Typography component="h2" variant="h3" align="left" color="textPrimary" gutterBottom>
+            Courses
+          </Typography>
+          <Typography component="h4" variant="h5" align="left" color="textPrimary" gutterBottom>
+            Add a course
+          </Typography>
+        </Box>
+        <Box m="1rem">
+          <form onSubmit={this.handleSubmit}>
+            <TextField
+              defaultValue={this.state.course.title}
+              onChange={this.handleChange}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="course-title"
+              label="Course Title"
+              name="course-title"
+              autoComplete="course-title"
+              autoFocus
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+            >
+              Save Course
+            </Button>
+          </form>
+        </Box>
+        <Box m="1rem">
+          
+        </Box>
+      </>
     )
   }
 }
